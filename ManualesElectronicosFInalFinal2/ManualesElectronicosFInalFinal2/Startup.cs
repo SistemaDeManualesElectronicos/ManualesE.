4dec3e61bd5834d6ba89b2223371d6e501c2757b
jesus moreno vazquez <hervey159@hotmail.com>
2020-02-11 18:13:14 -0600

    aplicando el routeo por defecto de mvc

--- ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Startup.cs
+++ ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Startup.cs
@@ -9,13 +9,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ManualesElectronicosFInalFinal2
 {
-    sopa do makako uma delicia
+   
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +27,8 @@ namespace ManualesElectronicosFInalFinal2
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
