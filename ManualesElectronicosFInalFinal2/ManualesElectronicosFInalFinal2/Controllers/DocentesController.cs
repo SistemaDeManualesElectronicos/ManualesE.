da60c04559a7f4ef0c77dd42cafc49bfec40d2e3
jesus moreno vazquez <hervey159@hotmail.com>
2020-02-13 17:12:35 -0600

    casi agregar

--- ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Controllers/DocentesController.cs
+++ ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Controllers/DocentesController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ManualesElectronicosFInalFinal2.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ManualesElectronicosFInalFinal2.Controllers
 {
     public class DocentesController : Controller
     {
+        DocentesRepository doc;
         public IActionResult Docentes()
         {
-            return View();
+            doc = new DocentesRepository();
+            return View(doc.GetDocentesxNombre());
         }
+
+
     }
 }
