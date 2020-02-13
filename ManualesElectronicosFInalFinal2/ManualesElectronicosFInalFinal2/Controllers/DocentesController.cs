a4e780e3d26def147dd9ffee0873128e51f4952c
jesus moreno vazquez <hervey159@hotmail.com>
2020-02-13 11:33:29 -0600

    organizando el proyecto

---
+++ ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Controllers/DocentesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManualesElectronicosFInalFinal2.Controllers
+{
+    public class DocentesController : Controller
+    {
+        public IActionResult Docentes()
+        {
+            return View();
+        }
+    }
+}
