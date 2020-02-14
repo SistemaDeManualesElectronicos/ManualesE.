26249f6ee3d147051f62f1a1d62b22f80b0d5321
jesus moreno vazquez <hervey159@hotmail.com>
2020-02-13 18:07:32 -0600

    Agregar

--- ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Controllers/DocentesController.cs
+++ ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Controllers/DocentesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ManualesElectronicosFInalFinal2.Models;
 using ManualesElectronicosFInalFinal2.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,29 @@ namespace ManualesElectronicosFInalFinal2.Controllers
         {
             doc = new DocentesRepository();
             return View(doc.GetDocentesxNombre());
+        }
+
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Agregar(Docentes nuevo)
+        {
+            doc = new DocentesRepository();
+            if (ModelState.IsValid)
+            {
+                doc.Insert(nuevo);
+              
+                return RedirectToAction("Docentes");
+            }
+            else
+            {
+                return View(nuevo);
+            }
+
         }
 
 
