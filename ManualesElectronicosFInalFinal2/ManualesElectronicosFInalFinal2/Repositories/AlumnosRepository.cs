5c0c1053046308267c25bb88af9ef61b426222d7
isaac <isaac@ISAAC-PORTATIL>
2020-03-05 16:35:21 -0600

    Lista Errores

--- ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Repositories/AlumnosRepository.cs
+++ ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Repositories/AlumnosRepository.cs
@@ -22,67 +22,113 @@ namespace ManualesElectronicosFInalFinal2.Repositories
 
         }
       
-      public bool validarAlumnosEditar (Alumnos alumnos)
-        {
-
-
-
-            return true;
-        }
+     
         sistemaselectronicoContext context = new sistemaselectronicoContext();
-       
-        public bool ValidarAlumnos(Alumnos alumnos)
+        Regex NombreConCaracteresEspecialess = new Regex(@"^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$");
+
+
+        public bool ErroresAgregar
         {
-          
-            Regex NombreConCaracteresEspeciales = new Regex(@"[ A-Za-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙ.-]+");
-            Regex NombreConCaracteresEspecialess = new Regex(@"^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$");
+
+            set
+            {
+       
+            }
+
+        }
+     
+        public List<string> ValidarAlumnos(Alumnos alumnos)
+        {
+            List<string> listaerrores = new List<string>();
             Regex NumeroDeControlA = new Regex(@"[1][0-9]{2}[A-Z]{1}[0]{1}[0-9]{3}");
 
-       //     if(alumnos.Nombre == null)
-       //     {
-       //         throw new Exception("El Nombre no puede ir vacio2");
-       //     }
+           
 
             if (string.IsNullOrWhiteSpace(alumnos.Nombre))
 
             {
-                throw new Exception("El Nombre no puede ir vacio");
+                listaerrores.Add("El nombre del alumno no puede ir vacio o tener espacios en blanco");
             }
-         //   if (!NombreConCaracteresEspeciales.IsMatch(alumnos.Nombre.ToString())) //validar que no haya caracteres especiales
-          //  {
-         //       throw new Exception("El Nombre no puede ir con caracteres especiales.");
-         //   }
+   
             if (!NombreConCaracteresEspecialess.IsMatch(alumnos.Nombre.ToString())) //validar que escriba apellidos
             {
-                throw new Exception("Verifique que haya escrito el nombre completo correctamente y no haya caracteres especiales");
+                listaerrores.Add("Verifique que haya escrito el nombre completo correctamente y no haya caracteres especiales");
+             
             }
-            if(alumnos.Nombre.Length >45)
+            if(alumnos.Nombre.Length >45)  
             {
-                throw new Exception("El limite para el nombre es de 45");
+                listaerrores.Add("El limite para el nombre es de 45");
+                
             }
-////////////////////////Numero Control
-            if (string.IsNullOrWhiteSpace(alumnos.NumeroControl.ToString()))  //Validar Numero Control vacio
 
+            if (Context.Alumnos.Any(x => x.Nombre == alumnos.Nombre))  //Validar si ya existe nombre de un alumno.
             {
-                throw new Exception("El Numero de control no puede ir vacio");
+                listaerrores.Add("Ya existe este Alumno, Por favor verifique sus datos");
+             
+            }
+            ////////////////////////Numero Control//////////////////////////////////////////////////////////////////
+            
+            if (string.IsNullOrWhiteSpace(alumnos.NumeroControl.ToString()))  //Validar Numero Control vacio
+            {
+                listaerrores.Add("El Numero de control no puede ir vacio");     
             }
             else
             {
                 if (!NumeroDeControlA.IsMatch(alumnos.NumeroControl.ToString())) //validar formato de numero de control
                 {
-                    throw new Exception("Numero de control incorrecto. Verifique que el formato este correcto");
+                    listaerrores.Add("Numero de control incorrecto. Verifique que el formato este correcto");
+                  
                 }
             }
-            if (alumnos.Nombre.Length > 8)
+            if (alumnos.NumeroControl.Length > 8 || alumnos.NumeroControl.Length < 8  )
             {
-                throw new Exception("Numero de control incorrecto. El limite para el nombre es de 8");
+                listaerrores.Add("Numero de control incorrecto. El Numero de contorl tiene 8 caracteres");
             }
+      
             if (Context.Alumnos.Any(x => x.NumeroControl == alumnos.NumeroControl))  //Validar si ya existe numero de control
             {
-                throw new Exception("Numero de control repetido");
+               
+                listaerrores.Add("Numero de control repetido");
             }
 
-            return true;
+
+     
+
+            return listaerrores ;
         }
+
+
+        public List<string> ValidarAlumnosEditar(Alumnos alumnos)
+        {
+            List<string> listaerrores = new List<string>();
+            if (string.IsNullOrWhiteSpace(alumnos.Nombre))
+
+            {
+                listaerrores.Add("El Nombre no puede ir vacio");
+
+            }
+            if (!NombreConCaracteresEspecialess.IsMatch(alumnos.Nombre.ToString())) //validar que escriba apellidos
+            {
+                listaerrores.Add("Verifique que haya escrito el nombre completo correctamente y no haya caracteres especiales");
+               
+            }
+            if (alumnos.Nombre.Length > 45)
+            {
+                listaerrores.Add("El limite para el nombre es de 45");
+
+            }
+
+            if (Context.Alumnos.Any(x => x.Nombre == alumnos.Nombre))  //Validar si ya existe nombre de un alumno.
+            {
+                listaerrores.Add("Ya existe este Alumno, Por favor verifique sus datos");
+               
+            }
+
+            return listaerrores;
+
+        }
+
+
+
     }
 }
