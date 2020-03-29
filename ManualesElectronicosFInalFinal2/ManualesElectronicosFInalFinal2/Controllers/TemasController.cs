77ba46b591025847ceb5f4927e7c77cdac22c593
jesus moreno vazquez <hervey159@hotmail.com>
2020-03-29 13:17:19 -0600

    //cambio validaciones

---
+++ ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Controllers/TemasController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManualesElectronicosFInalFinal2.Models;
+using ManualesElectronicosFInalFinal2.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManualesElectronicosFInalFinal2.Controllers
+{
+    public class TemasController : Controller
+    {
+        TemasRepository temas;
+        public IActionResult Temas()
+        {
+            temas = new TemasRepository();
+            return View(temas.GetTemascxNombre());
+        }
+
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Agregar(Temas t)
+        {
+            if (ModelState.IsValid)
+            {
+                temas = new TemasRepository();
+                List<string> errores = temas.Validacion(t);
+                if (errores.Count() == 0)
+                {
+                    temas.Insert(t);
+                    RedirectToAction("Temas");
+                }
+                else
+                {
+                    for (int i = 0; i < errores.Count(); i++)
+                    {
+                        ModelState.AddModelError("", errores[i]);
+
+                    }
+                    return View();
+                }
+
+            }
+
+            else
+            {
+                return View();
+            }
+            return View();
+        }
+
+        public IActionResult Eliminar(Temas d)
+        {
+            temas = new TemasRepository();
+            List<string> errores = temas.Validacion(d);
+            var datos = temas.GetTemabyId(d.Id);
+            return View(datos);
+        }
+
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+           temas = new TemasRepository();
+
+            var clase = temas.GetTemabyId(id);
+            temas.Delete(clase);
+            return RedirectToAction("Temas");
+        }
+
+
+        public IActionResult EditarTemas(int id)
+        {
+           temas = new TemasRepository();
+            var datos = temas.GetById(id);
+            return View(id);
+        }
+
+
+        public IActionResult EditarTemas(Temas d)
+        {
+
+            if (ModelState.IsValid)
+            {
+                temas = new TemasRepository();
+                List<string> errores = temas.Validacion(d);
+                for (int i = 0; i < errores.Count(); i++)
+                {
+                    ModelState.AddModelError("", errores[i]);
+                }
+
+                if (errores.Count() == 0)
+                {
+
+
+
+                    var Datos = temas.GetById(d.Id);
+                    Datos.Encabezado = d.Encabezado;
+                    Datos.IdSubTema = d.IdSubTema;
+                    temas.Update(Datos);
+                    return RedirectToAction("Temas");
+                }
+
+            }
+            else
+            {
+
+                return View(d);
+            }
+            return View(d);
+
+        }
+    }
+}
---
+++ ManualesElectronicosFInalFinal2/ManualesElectronicosFInalFinal2/Repositories/TemasRepository.cs
@@ -0,0 +1,48 @@
+﻿using ManualesElectronicosFInalFinal2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ManualesElectronicosFInalFinal2.Repositories
+{
+    public class TemasRepository:Repository<Temas>
+    {
+        public IEnumerable<Temas> GetTemascxNombre()
+        {
+            return Context.Temas.OrderBy(x => x.Encabezado);
+        }
+
+        public Temas GetTemabyId(int id)
+        {
+            return Context.Temas.FirstOrDefault(x=>x.Id == id);
+        }
+
+
+        public List<string> Validacion(Temas t)
+        {
+            List<string> pablito = new List<string>();
+            Regex NombreConCaracteresEspeciales = new Regex(@"^([A-Za-zÁÉÍÓÚñáéíóúÑ]){1,29}?$");
+            if (string.IsNullOrEmpty(t.Encabezado))
+            {
+                pablito.Add("rellene el pinche nombre");
+            }
+            else
+            {
+                if (!NombreConCaracteresEspeciales.IsMatch(t.Encabezado))
+                {
+                    pablito.Add("el encabezado es muy corto o no debe de llevar caracteres especiales");
+                }
+            }
+
+            if (Context.Temas.Any(x => x.Encabezado.ToUpper() == t.Encabezado.ToUpper()))
+            {
+                pablito.Add("El tema ya existe alv");
+            }
+           
+            return pablito;
+      
+        }
+    }
+}
